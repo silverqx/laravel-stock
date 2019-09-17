31fc3870973647d555840a7915d455701aaed701
silverqx <silver.zachara@gmail.com>
2019-09-17 14:06:37 +0200

    enhanced code assist for models

--- config/ide-helper.php
+++ config/ide-helper.php
@@ -51,7 +51,7 @@ return array(
     |
     */
 
-    'write_eloquent_model_mixins' => false,
+    'write_eloquent_model_mixins' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
     */
 
     'model_locations' => array(
-        'app',
+        'app/Modules',
     ),
 
 
